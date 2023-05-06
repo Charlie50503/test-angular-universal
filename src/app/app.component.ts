eb8ddfc5119f4089aad01de6f347dd2b7d9c0f07
charlie50503 <cate50503@gmail.com>
2023-05-06 20:16:35 +0800

    feat: 添加是否為瀏覽器判斷

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit, PLATFORM_ID } from '@angular/core';
 import { Store } from '@ngrx/store';
-
+import { isPlatformBrowser, isPlatformServer } from "@angular/common"
 import { LoggingService } from './logging.service';
 import * as fromApp from './store/app.reducer';
 import * as AuthActions from './auth/store/auth.actions';
@@ -14,10 +14,16 @@ export class AppComponent implements OnInit {
   constructor(
     private store: Store<fromApp.AppState>,
     private loggingService: LoggingService
+    , @Inject(PLATFORM_ID) private platformId
   ) {}
 
   ngOnInit() {
-    this.store.dispatch(new AuthActions.AutoLogin());
+    if (isPlatformBrowser(this.platformId)) {
+      this.store.dispatch(new AuthActions.AutoLogin());
+    }
     this.loggingService.printLog('Hello from AppComponent ngOnInit');
+
+    // this.store.dispatch(new AuthActions.AutoLogin());
+    // this.loggingService.printLog('Hello from AppComponent ngOnInit');
   }
 }
